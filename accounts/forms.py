097310cd74cb6c8fb6e30b252fcdef6c86913cac
min_2271 <min_2271@naver.com>
2018-07-12 12:08:23 +0900

    로그인 폼 코드 리팩토링

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,10 +2,7 @@ from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 class SignupForm(UserCreationForm):
-    
-    intra_id = forms.CharField()
-    intra_pw = forms.CharField()
-    
+     
     class Meta(UserCreationForm.Meta):
         # fields = ('username', 'email')
         fields = UserCreationForm.Meta.fields
@@ -13,13 +10,3 @@ class SignupForm(UserCreationForm):
     def save(self):
         user = super().save()
         return user
-
-
-class LoginForm(AuthenticationForm):
-    answer = forms.IntegerField(label='3+3?')
-
-    def clean_answer(self):
-        answer = self.cleaned_data.get('answer', None)
-        if answer != 6:
-            raise forms.ValidationError('mismatched!')
-        return answer
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,15 @@
 from django.conf import settings
 from django.shortcuts import render, redirect
 from django.contrib.auth.views import login as auth_login
-from .forms import SignupForm, LoginForm
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import SignupForm
 
 def signup(request):
     # 인트라넷 이메일 인증 필요
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
-            user = form.save()
+            user = form.save() # 인트라넷 아이디와 입력한 값이 같을 경우에만 가입이 가능하도록 처리
             return redirect(settings.LOGIN_URL)  # default: ' /accounts/login/'
     else:
         form = SignupForm()
@@ -19,8 +20,5 @@ def signup(request):
 
 def login(request):
     return auth_login(request,
-                authentication_form=LoginForm,
+                authentication_form=AuthenticationForm,
                 template_name='accounts/login_form.html')
-
-def logout(request):
-    pass
