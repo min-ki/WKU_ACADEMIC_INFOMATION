cbcdc5b9b3597f9452451dc7cc0e7decee4b53c5
min_2271 <min_2271@naver.com>
2018-07-17 23:23:58 +0900

    로그인 후 로그인 성공 메세지 출력

--- accounts/views.py
+++ accounts/views.py
@@ -7,11 +7,11 @@ def login(request):
     if request.method == 'POST':
         request.session['intranet_id'] = request.POST['username']
         request.session['intranet_pw'] = request.POST['password']
-        
+        messages.success(request, '로그인에 성공하셨습니다.')
         return redirect('home:index')
     else:
         request.session.flush()
-        
+    
     return render(request, 'accounts/login.html', {})
 
 # 세션 로그아웃
