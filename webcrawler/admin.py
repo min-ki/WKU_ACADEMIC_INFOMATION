3d66a9a74cf73a52ee7a8dc28774586bf3a7bc2a
min_2271 <min_2271@naver.com>
2018-07-20 01:42:18 +0900

    Admin 커스텀 및 webcrawler.models Subject 모델 학년, 이론, 실습, 세부전공 필드 추가 및 이름 변경

--- webcrawler/admin.py
+++ webcrawler/admin.py
@@ -4,8 +4,10 @@ from .models import Major, Subject
 
 @admin.register(Major)
 class MajorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'certification']
     search_fields = ['name']
 
 @admin.register(Subject)
 class SubjectAdmin(admin.ModelAdmin):
-    search_fields = ['title']
+    list_display = ['title', 'subject_grade', 'major', 'subject_type', 'subject_point']
+    search_fields = ['title', 'major__name']
--- webcrawler/models.py
+++ webcrawler/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Major(models.Model):
-    name = models.CharField(max_length=25) # 전공 명
+    name = models.CharField(max_length=30) # 전공 명
     certification = models.BooleanField(default=False) # 공학인증 여부
 
     def __str__(self):
@@ -16,6 +16,7 @@ class Subject(models.Model):
         ("선전", "선택전공"),
         ("응전", "응용전공"),
         ("계필", "계열필수"),
+        ('일선', '일반선택'),
     )
 
     CERTIFICATION_TYPE_CHOICES = (
@@ -26,13 +27,17 @@ class Subject(models.Model):
         ('인필BSM', '인필BSM'),
     )
 
-    title = models.CharField(max_length=25) # 제목
-    major = models.ManyToManyField(Major) # 학과
+    title = models.CharField(max_length=30) # 제목
+    major = models.ForeignKey(Major, on_delete=True) # 학과
     subject_type =  models.CharField(max_length=25, choices=SUBJECT_TYPE_CHOICES) # 수업유형
     certification_type = models.CharField(max_length=25, choices=CERTIFICATION_TYPE_CHOICES, blank=True) # 공학인증 유형
+    subject_grade = models.CharField(max_length=10) # 학년
     subject_number = models.IntegerField() # 학수번호
-    point = models.IntegerField() # 학점
+    subject_point = models.IntegerField() # 학점
+    subject_theory = models.CharField(max_length=10)  # 이론
+    subject_training = models.CharField(max_length=10)  # 실습
     necessary = models.BooleanField(default=False) # 필수과목 여부
+    subject_detail_major = models.CharField(max_length=30, blank=True) # 세부전공
 
     def __str__(self):
         return self.title
